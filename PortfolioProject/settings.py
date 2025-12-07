e9ec20ec3f265f57dd6547f9a112f68ae6af4bbc
Ragunandhan2001 <ragunandhanperumal@gmail.com>
2025-12-07 23:51:39 +0530

    Second Commit

--- PortfolioProject/settings.py
+++ PortfolioProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "PortfolioProject",
+    "Frontend",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = "PortfolioProject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "portfolio_db",
+        "USER" : "root",
+        "PASSWORD" : "Ragunandhan@123",
+        "HOST" : "localhost",
+        "PORT" : "3306"
     }
 }
 
